daa426a8ced232c0d3d5b21b47de461a598842b5
marryfeng <1473357859@qq.com>
2019-07-22 22:56:01 +0800

    服务的消费者

--- eurekaclient/src/main/java/com/learning/cn/eurekaclient/EurekaclientApplication.java
+++ eurekaclient/src/main/java/com/learning/cn/eurekaclient/EurekaclientApplication.java
@@ -2,12 +2,22 @@ package com.learning.cn.eurekaclient;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@EnableEurekaClient
+@RestController
 public class EurekaclientApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(EurekaclientApplication.class, args);
+    }
+
+    @RequestMapping("/hi")
+    public String hell(){
+        return  "hello,eureka!";
     }
 
 }
